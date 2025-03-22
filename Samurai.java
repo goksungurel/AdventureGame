fd8fa304bfec09b3591ec9897f91a4dc72cfe63f
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-22 23:35:13 +0300

    Create Samurai.java

---
+++ Samurai.java
@@ -0,0 +1,15 @@
+public class Samurai extends GameChar{
+    public Samurai() {
+        super(1,"Samurai",5,21,15);
+    }
+}
+class Archer extends GameChar{
+    public Archer(){
+        super(2,"Archer",7,18,20);
+    }
+}
+class Knight extends GameChar{
+    public Knight(){
+        super(3,"Knight",8,24,5);
+    }
+}
