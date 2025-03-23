c91522590c6d28af63da899ce4da6c2d39d361e4
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-23 22:54:31 +0300

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,19 +1,19 @@
 public class Inventory {
+    private Weapon weapon;
     private boolean food;
     private boolean water;
     private boolean firewood;
-    private String weaponName;
     private String armorName;
     private int weaponDamage;
     private int armorDefence;
 
     public Inventory() {
+        this.weapon=new Weapon("Yumruk ",2,56,40);
         this.food = false;
         this.water = false;
         this.firewood = false;
-        this.weaponName = "Yok";
         this.armorName = "Yok";
-        this.weaponDamage = 0;
+        this.weaponDamage = weapon.getDamage();
         this.armorDefence = 0;
     }
 
@@ -22,11 +22,18 @@ public class Inventory {
         this.food = food;
         this.water = water;
         this.firewood = firewood;
-        this.weaponName=weaponName;
         this.armorName=armorName;
         this.weaponDamage=weaponDamage;
         this.armorDefence=armorDefence;
     }
+    public Weapon getWeapon(){
+        return weapon;
+    }
+    public void setWeapon(Weapon weapon){
+        this.weapon=weapon;
+        this.weaponDamage=weapon.getDamage();
+    }
+    
     public boolean isFood(){
         return food;
     }
@@ -53,6 +60,21 @@ public class Inventory {
     }
     public void setArmorName(String armorName){
         this.armorName=armorName;
+    }
+    public int getWeaponDamage() {
+        return weaponDamage;
+    }
+
+    public void setWeaponDamage(int weaponDamage) {
+        this.weaponDamage = weaponDamage;
+    }
+
+    public int getArmorDefence() {
+        return armorDefence;
+    }
+
+    public void setArmorDefence(int armorDefence) {
+        this.armorDefence = armorDefence;
     }
 
 
