55905cf92d95fb07bdbdcdc3fb93a9fc43dc34c3
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-23 22:41:45 +0300

    Update ToolStore.java

--- ToolStore.java
+++ ToolStore.java
@@ -16,19 +16,55 @@ public class ToolStore extends NormalLoc{
         switch(selectCase){
             case 1:
                 printWeapon();
+                buyWeapon();
                 break;
             case 2:
                 printArmor();
+                buyWeapon();
                 break;
             case 3:
-                return true;
+                return false;
         }
 
 
         return true;
     }
-    public void printWeapon(){
+       public void printWeapon(){
         System.out.println("Weapons");
+        System.out.println();
+        for(Weapon w: Weapon.weapons()){
+            System.out.println("ID: " + w.getId() +
+                    " | Name: " + w.getName() +
+                    " | Damage: " + w.getDamage() +
+                    " | Price: " + w.getPrice());
+
+        }
+
+    }
+    public void buyWeapon(){
+        System.out.println("Please choose a weapon by entering its ID");
+        int selectWeaponID=scanner.nextInt();
+        while(selectWeaponID <1 || selectWeaponID >Weapon.weapons().length){
+            System.out.println("You have entered an invalid option! Please enter a valid option.");
+            selectWeaponID =scanner.nextInt();
+        }
+
+        Weapon selectedWeapon =Weapon.getWeaponObjByID(selectWeaponID);
+        if(selectedWeapon !=null) {
+            if (selectedWeapon.getPrice() > this.getPlayer().getMoney()) {
+                System.out.println("You have not a enough money! ");
+            } else {
+                //Purchase logic
+                System.out.println(selectedWeapon.getName() + "You have purchased the  weapon");
+                double balance = this.getPlayer().getMoney() - selectedWeapon.getPrice();
+                this.getPlayer().setMoney(balance);
+                System.out.println("Your remaining money is: " + this.getPlayer().getMoney());
+                System.out.println("Previous weapon is: "+this.getPlayer().getInventory().getWeapon().getName());
+                this.getPlayer().getInventory().setWeapon(selectedWeapon);
+                System.out.println("New weapon is: "+this.getPlayer().getInventory().getWeapon().getName());
+
+            }
+        }
     }
     public void printArmor(){
         System.out.println("Armor");
