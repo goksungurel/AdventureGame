7b0af27c97af67e735ff14e8d0b5644fb5712f1a
goksungurel <148645864+goksungurel@users.noreply.github.com>
2025-03-25 13:08:07 +0300

    Create Zombie.java

---
+++ Zombie.java
@@ -0,0 +1,15 @@
+public class Zombie extends Obstacle{
+    public Zombie() {
+        super("Zombie",1,20,100);
+    }
+}
+class Vampire extends Obstacle{
+    public Vampire() {
+        super("Vampire",2,30,120);
+    }
+}
+class Bear extends Obstacle{
+    public Bear() {
+        super("Bear",3,40,130);
+    }
+}
